9504273b1dcdf278bbfddb71e82abe8fda231387
kevinab107 <kevinab107@gmail.com>
2019-11-01 14:51:05 +0530

    Check for valid rotations
    Ensure that there are no rabi rotation and detuning rotation at the same offset

--- environment/neural_network.py
+++
@@ -1,2 +0,0 @@
-# To be implemented with Tensorflow and Keras. DQN for the agent with the cost function corresponding to the fidelity vector sum minimum.
-# Algorithm detailed in the pdf RL_alogorithm_approach
