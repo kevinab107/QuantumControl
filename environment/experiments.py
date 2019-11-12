9ff1cafcbc8f7a6eb0b2f51561b10918b6164c91
kevinab107 <kevinab107@gmail.com>
2019-11-12 09:36:01 +0530

    [06][Kevin Abraham] Algorithm
    1) Algorithm (Rollout) with value approximation

--- environment/experiments.py
+++ environment/experiments.py
@@ -66,29 +66,29 @@ control_omega_y =[]
 
 # check whether a segment is valid. True if the control 
 # has been determined for the segment
-def is_valid_segment(segment):
-    return segment <= get_current_segment()
+# def is_valid_segment(segment):
+#     return segment <= get_current_segment()
 
-def get_detuning_control(t):
-    segment = int(t/get_segment_duration())
-    if(is_valid_segment(segment)):
-        return control_detuning[segment]
-    else:
-        return 0
+# def get_detuning_control(t):
+#     segment = int(t/get_segment_duration())
+#     if(is_valid_segment(segment)):
+#         return control_detuning[segment]
+#     else:
+#         return 0
 
-def get_omega_x_control(t):
-    segment = int(t/get_segment_duration())
-    if(is_valid_segment(segment)):
-        return control_omega_x[segment]
-    else:
-        return 0
+# def get_omega_x_control(t):
+#     segment = int(t/get_segment_duration())
+#     if(is_valid_segment(segment)):
+#         return control_omega_x[segment]
+#     else:
+#         return 0
 
-def get_omega_y_control(t):
-    segment = int(t/get_segment_duration())
-    if(is_valid_segment(segment)):
-        return control_omega_y[segment]
-    else:
-        return 0
+# def get_omega_y_control(t):
+#     segment = int(t/get_segment_duration())
+#     if(is_valid_segment(segment)):
+#         return control_omega_y[segment]
+#     else:
+#         return 0
 
 #update the control list with the latest 
 def append_controls(detuning, omega_x, omega_y):
@@ -97,7 +97,11 @@ def append_controls(detuning, omega_x, omega_y):
     control_omega_x.append(omega_x)
     control_omega_y.append(omega_y)
 
-append_controls(1,1,1)
+def clear_controls():
+    control_detuning.clear()
+    control_omega_x.clear()
+    control_omega_y.clear()
+
 #%%
 # Hamiltonian 
 h =   1   # 2*np.pi*6.6*1e-36  # planks constatn 
@@ -105,41 +109,42 @@ w0 = .5   # static magnetic field in T
 w =  .4   # driving frequency
 
 detuning =  w0 - w 
-def detuning(t, args):
-    return h/2*(w0 - get_detuning_control(t))
-def Omega_x(t, args):
-    return get_omega_x_control(t)*h/2     # Rabi rate  along x 
+# def detuning(t, args):
+#     return h/2*(w0 - get_detuning_control(t))
+# def Omega_x(t, args):
+#     return get_omega_x_control(t)*h/2     # Rabi rate  along x 
 
-def Omega_y(t, args):
-    return get_omega_y_control(t)*h/2     # Rabi rate  along y
+# def Omega_y(t, args):
+#     return get_omega_y_control(t)*h/2     # Rabi rate  along y
 
-def noise(t, args):  
-    #TODO [ALGORITHM]:  make this staochastic using random variable for each segment
-    return np.sin(10*t)*10*h/2
+# def noise(t, args):  
+#     #TODO [ALGORITHM]:  make this staochastic using random variable for each segment
+#     return np.sin(10*t)*10*h/2
 
-H_0 = q.sigmaz()
-H_noise = q.sigmam()
-H_x = q.sigmax()
-H_y = q.sigmay() 
+# H_0 = q.sigmaz()
+# H_noise = q.sigmam()
+# H_x = q.sigmax()
+# H_y = q.sigmay() 
 
-#H  = h/2*(detuning*q.sigmaz() + noise*q.sigmaz() + Omega_x*q.sigmax() + Omega_y*q.sigmay())
-H = [[H_0, detuning], [H_noise, noise], [H_x, Omega_x], [H_y, Omega_y]]
+# #H  = h/2*(detuning*q.sigmaz() + noise*q.sigmaz() + Omega_x*q.sigmax() + Omega_y*q.sigmay())
+# H = [[H_0, detuning], [H_noise, noise], [H_x, Omega_x], [H_y, Omega_y]]
 
-#%% Simulation 
-psi0 = q.basis(2,0) 
-TIME_UPPER_BOUND  = 2
-segments = TIME_UPPER_BOUND/get_segment_duration()
-t = np.arange(0,.5,.1)
-c_ops = []
-append_controls(.4,5,5)
-output = q.mesolve(H, psi0, t, c_ops)
+# #%% Simulation 
 
-# %% Visualize the evolution 
-sphere = q.Bloch()
-for states in output.states:
-    sphere.clear()
-    sphere.add_states(states)
-    sphere.save(dirc = 'temp')
+# psi0 = q.basis(2,0) 
+# TIME_UPPER_BOUND  = 2
+# segments = TIME_UPPER_BOUND/get_segment_duration()
+# t = np.arange(0,.5,.1)
+# c_ops = []
+# append_controls(.4,5,5)
+# output = q.mesolve(H, psi0, t, c_ops)
+
+# # %% Visualize the evolution 
+# sphere = q.Bloch()
+# for states in output.states:
+#     sphere.clear()
+#     sphere.add_states(states)
+#     sphere.save(dirc = 'temp')
 
 # %%
 
@@ -224,6 +229,8 @@ def get_control_cordinates_3d():
 #%%
 
 ## ordered nearnest neighbours for fine tuning
+k_value = 100
+
 def get_ordered_knn_map(k_value):
     
     coordinates = get_control_cordinates_3d()
@@ -250,5 +257,102 @@ def get_ordered_kfn_map(k_value):
         ordered_neighbours_map[current_coordinate] = sorted_neighbours
     return ordered_neighbours_map
 
+ordered_knn_map = get_ordered_knn_map(k_value)
+ordered_kfn_map = get_ordered_knn_map(k_value)
+
+def get_next_actions(current_controls):
+    current_controls = str(current_controls)
+    current_fidelity = get_fidelity(current_state, target_state)
+    if(is_finetuning_required(current_fidelity)):
+        possible_controls  = ordered_knn_map[current_controls]
+    else:
+        possible_controls = ordered_kfn_map[current_controls]
+    
+    return possible_controls
+
 ## TODO [OOP] : Convert the control space functionalities to a Class and define the relevant get and 
 # set methods
+
+#%%
+from queue import PriorityQueue
+## Define the operation to be performed 
+target_unitary = q.sigmax()
+current_state = q.basis(2,0)
+target_state = target_unitary*current_state
+
+## Set the threshold to start fine tuning [with respect to fidelity] 
+fine_tuning_treshold = .9999999601441566
+# pick the user defined intial control (default 0 0 0). Based on the intial control we find the k controls from knn or kfn for the fist time segment
+initial_control = '[0. 0. 0.]'
+
+h =   1   # 2*np.pi*6.6*1e-36  # planks constatn 
+w0 = .5   # static magnetic field in T 
+w =  .4   # driving frequency
+
+detuning =  w0 - w 
+
+def noise(t, args):  
+    #TODO [ALGORITHM]:  make this staochastic using random variable for each segment
+    return np.sin(10*t)*10*h/2
+
+H_0 = q.sigmaz()
+H_noise = q.sigmam()
+H_x = q.sigmax()
+H_y = q.sigmay() 
+
+def is_finetuning_required(fidelity):
+    return fidelity > fine_tuning_treshold
+
+def get_cost_to_go(fidelity):
+    if(fidelity == 0 ):
+        cost = float('inf')
+    else: 
+        cost = 1/fidelity  -1 
+    return cost
+
+def get_next_state(current_state):
+   current_control = initial_control
+   actions = get_next_actions(current_control)
+   print("number of actions :", len(actions))
+   minimum_cost = float('inf')
+  
+   next_state = q.basis(2,0)
+   preferred_action = current_control
+   action_cost = 0 
+   
+ 
+   for action in actions : 
+       detuning = action[0]
+       omx  = action[1]
+       omy  = action[2]
+       H_control = (h/2)*(detuning*H_0 + omx*H_x + omy*H_y )
+       H = [H_control, [H_noise, noise]]
+       t = np.arange(0,get_segment_duration(),.1)
+       transition_state = q.mesolve(H, current_state, t, []).states[-1]
+       #TODO : add the eucledian distance to the action cost. Improve the logic for bias
+       action_cost += 1
+       fidelity = get_fidelity(transition_state, target_state)
+       if(fidelity):
+         action_cost += 1 
+       else:
+         action_cost = 0 
+       Q = get_cost_to_go(fidelity)  + action_cost
+       if(Q < minimum_cost):
+            minimum_cost = Q
+            next_state = transition_state
+            preferred_action = action
+
+   append_controls(preferred_action[0], preferred_action[1], preferred_action[2])
+   return next_state
+
+def get_fidelity(state_i , state_f):
+    return (state_i.dag()*state_f).norm()
+
+fidelity = get_fidelity(current_state, target_state)
+states = []
+while(fidelity < 1):
+    states.append(current_state)
+    current_state = get_next_state(current_state)
+    fidelity = get_fidelity(current_state, target_state)
+    print(current_state, fidelity)
+#%%
